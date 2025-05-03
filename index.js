e45200e83bea313a82e6c1f28f0c85ffa40e9ee4
Kaylin98 <kaylinmaharaj@gmail.com>
2025-05-03 17:31:04 +0200

    update:fix: update Redis client initialization for Redis v4+

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const redis = require('redis');
-
+const express = require("express");
+const redis = require("redis");
+ 
 const app = express();
 const client = redis.createClient({
-    host: 'redis-server',
-    port: 6379
+  url: "redis://redis-server:6379",
 });
-client.set('visits',0)
-
-app.get('/',(req,res) => {
-    client.get('visits',(err,visits) => {
-        res.send('Number of visits is ' + visits);
-        client.set('visits', parseInt(visits) + 1);
-    });
+ 
+(async () => {
+  await client.connect();
+  await client.set("visits", 0);
+})();
+ 
+app.get("/", async (req, res) => {
+  const visits = await client.get("visits");
+  res.send("Number of visits " + visits);
+  await client.set("visits", parseInt(visits) + 1);
 });
-
-
-app.listen(8081, () =>{
-    console.log('Listening on port 8081');
+ 
+app.listen(8081, () => {
+  console.log("listening on port 8081");
 });
